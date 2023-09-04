889aae70b4dcd1c52970697f8e03c761d93a0c8b
barnabas <barnascript@gmail.com>
2023-09-04 15:24:27 +0100

    initial commit and header done

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,62 @@
+import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import star_logo from "../../assets/icons/star-logo.svg";
+import { LiaTimesSolid } from "react-icons/lia";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
+
+const Header = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <div className={`fw ${styles.wrapper}`}>
+      <div className={`sw ${styles.container}`}>
+        <div className={styles.logo}>
+          <img src={star_logo} alt="logo" />
+        </div>
+        <nav>
+          <div className={styles.links}>
+            <Link className={` meta_text ${styles.link}`}>WORK</Link>
+            <Link className={` meta_text ${styles.link}`}>CONTACT</Link>
+          </div>
+          <button className={`meta_small ${styles.button}`}>
+            <div className={`meta_small ${styles.button_line}`}></div>
+            <span>GET IN TOUCH</span>
+          </button>
+        </nav>
+      </div>
+      <div className={`sw ${styles.mobile_container}`}>
+        <div className={styles.logo}>
+          <img src={star_logo} alt="logo" />
+        </div>
+        <div className={styles.togglers}>
+          {!toggleMenu && (
+            <RxHamburgerMenu
+              className={styles.open_toggle}
+              onClick={() => setToggleMenu(!toggleMenu)}
+            />
+          )}
+          {toggleMenu && (
+            <LiaTimesSolid
+              className={styles.close_toggle}
+              onClick={() => setToggleMenu(!toggleMenu)}
+            />
+          )}
+        </div>
+        {toggleMenu && (
+          <nav>
+            <div className={styles.links}>
+              <Link className={` meta_text ${styles.link}`}>WORK</Link>
+              <Link className={` meta_text ${styles.link}`}>CONTACT</Link>
+            </div>
+            <button className={`meta_small ${styles.button}`}>
+              <div className={`meta_small ${styles.button_line}`}></div>
+              <span>GET IN TOUCH</span>
+            </button>
+          </nav>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
